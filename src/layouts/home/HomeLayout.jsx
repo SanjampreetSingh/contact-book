3b37b74c14de27eaf0a4da3e2aa8791b436ece12
Sanjampreet Singh <sanjam531@gmail.com>
2022-01-26 18:32:22 +0530

    removed redundent code

--- src/layouts/home/HomeLayout.jsx
+++ src/layouts/home/HomeLayout.jsx
@@ -1,19 +1,13 @@
 import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
 
-import Header from "../../components/common/header/Header"
-
-// import Footer from "../../components/common/footer/Footer"
-
 export default function HomeLayout(props) {
   const { children } = props
   return (
     <>
-      <Header />
       <Container maxWidth="lg">
         <Box sx={{ my: 4, py: 2 }}>{children}</Box>
       </Container>
-      {/* <Footer /> */}
     </>
   )
 }
